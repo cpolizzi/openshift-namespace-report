ae690bd3b27ccef45e1cfbe4f144ddabd3c75d0e
Christian Polizzi <christian.polizzi@redhat.com>
2023-09-26 02:14:20 +0000

    Logging
    Added logging.
    Improved feedback.

--- generate-namespace-resources-report.sh
+++ generate-namespace-resources-report.sh
@@ -3,12 +3,28 @@
 # References:
 # - https://access.redhat.com/solutions/3986301
 
-now=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-data_file=reports/namespace-resources-report.${now}.csv
-mkdir -p $(dirname ${data_file})
-exec 1>>"${data_file}"
+function get_timestamp_format() {
+    local result="%Y-%m-%dT%H:%M:%SZ"
+
+    echo ${result}
+}
+
+function log() {
+    local now=$(date -u +"$(get_timestamp_format)")
+
+    echo "[${now}] - $@" >/dev/tty
+}
+
+# Prepare for report data capture
+report_timestamp=$(date -u +"$(get_timestamp_format)")
+report_file=reports/namespace-resources-report.${report_timestamp}.csv
+mkdir -p $(dirname ${report_file})
+
+# Redirect stdout to the report file
+exec 1>>"${report_file}"
 
 # Retrieve from every namespace all resources
+log "Retrieving all resources from all namespaces"
 echo "Namespace,Kind,Name,Timestamp"
 oc get \
     $(oc api-resources --namespaced=true --verbs=list -o name | awk '{printf "%s%s",sep,$0;sep=","}') \
@@ -18,6 +34,9 @@ oc get \
     --sort-by='metadata.namespace'
 
 # Filter out system namespaces
-mv ${data_file} ${data_file}.0
-egrep -v '^(default,|kube-|openshift,|openshift-)' ${data_file}.0 > ${data_file}
-rm -f ${data_file}.0
+log "Filtering out system namespace resources"
+mv ${report_file} ${report_file}.0
+egrep -v '^(default,|kube-|openshift,|openshift-)' ${report_file}.0 > ${report_file}
+rm -f ${report_file}.0
+
+log "Report is located in ${report_file}"
